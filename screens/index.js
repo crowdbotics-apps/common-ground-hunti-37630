3df8595b263f68374a6907326d92f8732cfd3196
Crowdbotics <team@crowdbotics.com>
2022-12-02 19:11:03 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,343 +1,153 @@
 import React, { useState, useEffect } from "react";
 import {
   Text,
-  View,
   StyleSheet,
-  Pressable,
+  View,
   TextInput,
-  Image,
-  ScrollView
+  Pressable,
+  Image
 } from "react-native";
 
-const Feedback = () => {
-  const [message, setMessage] = useState("");
-  const [reviews, setReviews] = useState([]);
-  const [expanded, setExpanded] = useState(null);
+const CreatePostScreen2 = (params) => {
+  const [caption, setCaption] = useState("");
+  const [tags, setTags] = useState("");
+  const [username, setUsername] = useState("");
+  const [date, setDate] = useState("18 June 2022");
+  const [time, setTime] = useState("13:00 PM");
+  const [images, setImages] = useState([]);
   useEffect(() => {
-    setReviews([
-      {
-        id: 1,
-        username: "Username",
-        email: "username@email.com",
-        image: require("./assets/userImage.png"),
-        subject: "Molestie vestibulum nulla.",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium, commodo lacus, amet nulla faucibus vulputate erat vestibulum. Aliquet consequat nunc sit ullamcorper vel egestas nunc sagittis lectus. Sed ipsum vel in morbi non semper adipiscing nibh nam. Integer sem."
-      },
-      {
-        id: 2,
-        username: "Username",
-        email: "username@email.com",
-        image: require("./assets/userImage.png"),
-        subject: "Molestie vestibulum nulla.",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium, commodo lacus, amet nulla faucibus vulputate erat vestibulum. Aliquet consequat nunc sit ullamcorper vel egestas nunc sagittis lectus. Sed ipsum vel in morbi non semper adipiscing nibh nam. Integer sem."
-      },
-      {
-        id: 3,
-        username: "Username",
-        email: "username@email.com",
-        image: require("./assets/userImage.png"),
-        subject: "Molestie vestibulum nulla.",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium, commodo lacus, amet nulla faucibus vulputate erat vestibulum. Aliquet consequat nunc sit ullamcorper vel egestas nunc sagittis lectus. Sed ipsum vel in morbi non semper adipiscing nibh nam. Integer sem."
-      },
-      {
-        id: 4,
-        username: "Username",
-        email: "username@email.com",
-        image: require("./assets/userImage.png"),
-        subject: "Molestie vestibulum nulla.",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium, commodo lacus, amet nulla faucibus vulputate erat vestibulum. Aliquet consequat nunc sit ullamcorper vel egestas nunc sagittis lectus. Sed ipsum vel in morbi non semper adipiscing nibh nam. Integer sem."
-      }
+    setUsername("Username");
+    setDate("18 June 2022");
+    setTime("13:00 PM");
+    setImages([
+      require("./assets/crowdboticsImage.png"),
+      require("./assets/crowdboticsImage.png"),
+      require("./assets/crowdboticsImage.png"),
+      require("./assets/crowdboticsImage.png")
     ]);
   }, []);
-  useEffect(() => {
-    setExpanded(reviews[3]);
-  }, [reviews]);
-  const handleExpand = item => {
-    if (expanded === item) {
-      setExpanded(null);
-    } else {
-      setExpanded(item);
-    }
-  };
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <TabView
-          tabTitles={["All Users"]}
-          selected={0}
-          style={styles.tabView}
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Caption</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setCaption(text)}
+          value={caption}
+          placeholder="Enter your caption"
+          placeholderTextColor="#9B9B9B"
+          autoCapitalize="none"
+          autoCorrect={false}
         />
-        <Text style={styles.title}>Feedback</Text>
-        <View style={styles.feedbackContainer}>
-          {reviews.map((review, index) => (
-            <Review
-              key={index}
-              item={review}
-              expanded={review === expanded}
-              onPress={x => {
-                handleExpand(x);
-              }}
-            />
-          ))}
+      </View>
+      <Text style={styles.inputText}>Add Image</Text>
+      <View style={styles.addImages}>
+        {images.map((image, index) => (
+          <Image source={image} style={styles.image} key={index} />
+        ))}
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Add Tags</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setTags(text)}
+          value={tags}
+          placeholder="Enter your caption"
+          placeholderTextColor="#9B9B9B"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+      </View>
+      <View style={styles.userInfo}>
+        <Text>{username}</Text>
+        <View style={styles.dateTime}>
+          <Text>{date}</Text>
+          <Text>, </Text>
+          <Text>{time}</Text>
         </View>
-      </ScrollView>
-      <View style={styles.footer}>
-        <View style={styles.camera}>
-          <Image
-            source={require("./assets/cameraIcon.png")}
-            style={styles.cameraIcon}
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Type a message"
-            onChangeText={text => setMessage(text)}
-            value={message}
-            autoCorrect={false}
-            autoCapitalize="none"
-            autoFocus={false}
-          />
-          <Image
-            source={require("./assets/emojiIcon.png")}
-            style={styles.smileyIcon}
-          />
-          <Image
-            source={require("./assets/voiceIcon.png")}
-            style={styles.voiceIcon}
-          />
-        </View>
-        <View style={styles.send}>
-          <Image
-            source={require("./assets/sendIcon.png")}
-            style={styles.sendIcon}
-          />
-        </View>
+      </View>
+      <View style={styles.btnContainer}>
+        <Pressable style={styles.btn}>
+          <Text style={styles.btnText}>Submit</Text>
+        </Pressable>
       </View>
     </View>
   );
 };
-
-const Review = ({ item, expanded, onPress }) => {
-  return (
-    <Pressable style={reviewStyles.container} onPress={() => onPress(item)}>
-      <View style={reviewStyles.header}>
-        <Image source={item.image} style={reviewStyles.image} />
-        <View style={reviewStyles.info}>
-          <Text style={reviewStyles.username}>{item.username}</Text>
-          <Text style={reviewStyles.email}>{item.email}</Text>
-        </View>
-        <Image
-          source={require("./assets/menuIcon.png")}
-          style={reviewStyles.menuIcon}
-        />
-      </View>
-      {expanded
-        ? (
-        <View style={reviewStyles.body}>
-          <Text style={reviewStyles.detailsText}>
-            <Text style={reviewStyles.green}>Email: {"\t"}</Text>
-            {"\t"}
-            {item.email}
-          </Text>
-          <Text style={reviewStyles.detailsText}>
-            <Text style={reviewStyles.green}>Subject: </Text>
-            {"\t"}
-            {item.subject}
-          </Text>
-          <Text style={reviewStyles.description}>{item.description}</Text>
-          <Pressable style={reviewStyles.btn}>
-            <Text style={reviewStyles.btnText}>Reply</Text>
-          </Pressable>
-        </View>
-          )
-        : null}
-    </Pressable>
-  );
-};
-
-const reviewStyles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    backgroundColor: "#f1f1f1",
-    borderRadius: 10,
-    marginHorizontal: 20,
-    marginBottom: 10
-  },
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between"
-  },
-  image: {
-    width: 50,
-    height: 50,
-    borderRadius: 25
-  },
-  menuIcon: {
-    width: 20,
-    height: 20
-  },
-  username: {
-    fontSize: 14
-  },
-  email: {
-    fontSize: 12
-  },
-  info: {
-    flexDirection: "column",
-    flex: 1,
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    marginHorizontal: 10,
-    height: 40
-  },
-  body: {
-    marginTop: 10,
-    paddingTop: 10,
-    borderTopWidth: 1,
-    borderTopColor: "#ccc"
-  },
-  detailsText: {
-    marginBottom: 5
-  },
-  green: {
-    color: "#12D790"
-  },
-  description: {
-    fontSize: 12,
-    marginTop: 5,
-    textAlign: "justify"
-  },
-  btn: {
-    backgroundColor: "#000",
-    width: 60,
-    height: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 10,
-    marginVertical: 5,
-    alignSelf: "flex-end"
-  },
-  btnText: {
-    color: "#fff",
-    fontSize: 12
-  }
-});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
-  },
-  tabView: {
-    width: 150,
-    marginLeft: 20,
-    marginVertical: 10
-  },
-  title: {
-    marginLeft: 20,
-    marginBottom: 10,
-    marginTop: 20
-  },
-  footer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
-    margin: 10
+    backgroundColor: "#fff",
+    paddingHorizontal: 20
   },
   inputContainer: {
-    width: "80%"
+    flexDirection: "column",
+    justifyContent: "center" // marginHorizontal: 5,
+  },
+  inputText: {
+    fontSize: 16,
+    marginLeft: 20,
+    color: "#111112"
   },
   input: {
-    paddingLeft: 15,
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
     borderRadius: 10,
-    backgroundColor: "#F1F1F1",
-    height: 45
+    padding: 10,
+    paddingLeft: 20,
+    marginVertical: 10,
+    width: "100%",
+    height: 150
   },
-  smileyIcon: {
-    position: "absolute",
-    right: 40,
-    top: 10,
-    opacity: 0.5
-  },
-  voiceIcon: {
-    top: 12,
-    right: 15,
-    position: "absolute",
-    opacity: 0.5
-  }
-});
-
-export default Feedback;
-
-const TabView = ({
-  tabTitles,
-  selected,
-  onPress,
-  tabColor,
-  backgroundColor,
-  style
-}) => {
-  const tabColorStyle = {
-    backgroundColor: tabColor || "#fff"
-  };
-  const backgroundColorStyle = {
-    backgroundColor: backgroundColor || "#F1F1F1"
-  };
-  const propStyle = style || {};
-  return (
-    <View
-      style={[tabViewStyles.paletteContainer, backgroundColorStyle, propStyle]}>
-      {tabTitles.map((title, index) => (
-        <Pressable
-          onPress={() => (onPress ? onPress(index) : null)}
-          style={
-            index === selected
-              ? [tabViewStyles.selected, tabColorStyle]
-              : [tabViewStyles.unSelected, backgroundColorStyle]
-          }
-          key={index}>
-          <Text>{title}</Text>
-        </Pressable>
-      ))}
-    </View>
-  );
-};
-
-const tabViewStyles = StyleSheet.create({
-  paletteContainer: {
-    width: "80%",
-    height: 48,
-    backgroundColor: "#E4E4E4",
+  addImages: {
+    paddingVertical: 10,
     flexDirection: "row",
     alignItems: "center",
-    borderRadius: 10,
-    padding: 6,
-    marginVertical: 10
+    justifyContent: "center"
   },
-  selected: {
+  image: {
+    height: 80,
+    width: 80,
+    marginHorizontal: 5,
     borderRadius: 10,
-    flex: 1,
-    backgroundColor: "#fff",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "gray",
-    elevation: 10
+    alignSelf: "center"
   },
-  unSelected: {
-    flex: 1,
-    height: "100%",
+  list: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "flex-end"
+  },
+  userInfo: {
+    marginTop: 20,
+    paddingHorizontal: 10
+  },
+  dateTime: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center"
+  },
+  btnContainer: {
+    padding: 30,
+    paddingTop: 10,
+    paddingHorizontal: 40,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#E4E4E4",
-    borderRadius: 10
+    marginTop: 20
+  },
+  btn: {
+    backgroundColor: "black",
+    height: 50,
+    width: "100%",
+    padding: 10,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold"
   }
 });
+export default CreatePostScreen2;
