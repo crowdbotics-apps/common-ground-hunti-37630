87617ea43fcc679fef0167a95589cb300357bcea
Crowdbotics <team@crowdbotics.com>
2022-12-02 19:10:56 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,25 +1,68 @@
 import React from "react";
-import { Text, StyleSheet, View, Image, Pressable } from "react-native";
+import { Text, StyleSheet, View, Image, TextInput, TouchableHighlight, ScrollView } from "react-native";
 
-const UploadMediaScreen = (params) => {
+const PlaceBidScreen = (params) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.menu}>
-        <View style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Take photo or video</Text>
-          <Image source={require("./assets/cameraIcon.png")} />
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("./assets/back.png")} style={styles.back} />
+        <Text style={styles.heading}>Place a bid</Text>
+        <Text />
+      </View>
+      <Text style={styles.subHeading}>Current Balance</Text>
+      <View style={styles.totalAmount}>
+        <View style={styles.amountText}>
+          <Text style={styles.priceText}>1256 ETH</Text>
+          <Text style={styles.walletAdd}>Wallet address: 0x25Ddgf546548564</Text>
         </View>
-        <View style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Photo Library</Text>
-          <Image source={require("./assets/libraryIcon.png")} />
-        </View>
-        <View style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Browse</Text>
-          <Image source={require("./assets/menuIcon.png")} />
+        <Image source={require("./assets/eth.png")} style={styles.ethImg} />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.mr10}>Name of the NFT</Text>
+        <Input placeholder="Enter" />
+      </View>
+
+      <Text style={styles.mt10}>Enter quantity - Available</Text>
+      <View style={[styles.chooseContainer, { justifyContent: "space-evenly" }]}>
+        <Image source={require("./assets/minus.png")} style={styles.priceImg} />
+        <TextInput placeholder='2.75 ETH' placeholderTextColor="#000" style={styles.inputPrice} />
+        <Image source={require("./assets/plus.png")} style={styles.priceImg} />
+      </View>
+      <View style={styles.bidContainer}>
+        <Text style={styles.bidText}>You must bid higher than 2.75 ETH</Text>
+      </View>
+      <Text style={styles.mt10}>Amount of bid</Text>
+      <View style={[styles.chooseContainer, { justifyContent: "space-evenly" }]}>
+        <Image source={require("./assets/minus.png")} style={styles.priceImg} />
+        <TextInput placeholder='1' placeholderTextColor="#000" style={styles.inputPrice} />
+        <Image source={require("./assets/plus.png")} style={styles.priceImg} />
+      </View>
+      <View style={styles.bidContainer}>
+        <Text style={styles.bidText}>Quantity</Text>
+      </View>
+
+      <View style={styles.feeContainer}>
+      <View>
+        <Text style={[styles.mr10, { marginLeft: 15 }]}>Service fee</Text>
+        <View style={styles.feeSection}>
+          <Text>0.25 ETF</Text>
         </View>
       </View>
-      <Button buttonText="Cancel" outline={true} />
-    </View>
+      <View>
+        <Text style={[styles.mr10, { marginLeft: 15 }]}>You will pay </Text>
+        <View style={styles.feeSection}>
+          <Text>3.14 ETH</Text>
+        </View>
+      </View>
+      </View>
+
+      <View style={styles.buttonBottom}>
+        <Button>Place a Bid</Button>
+        <Button backgroundColor="#fff" color="#000" borderWidth={1} >
+          Cancel
+        </Button>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -27,73 +70,167 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    padding: 20,
-    justifyContent: "flex-end"
+    paddingHorizontal: 10
   },
-  menuItem: {
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 30,
+    marginTop: 10,
+    marginBottom: 40
+  },
+  back: { width: 11.25, height: 20, resizeMode: "contain", marginLeft: -20 },
+  heading: { fontSize: 16, color: "#000", marginLeft: 20 },
+  subHeading: { fontSize: 16, fontWeight: "400", marginLeft: 13 },
+  totalAmount: {
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 15,
     justifyContent: "space-between",
-    borderBottomWidth: 1,
-    borderColor: "#ccc"
+    width: "100%",
+    marginTop: 5
   },
-  menuItemText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#000",
-    textAlign: "left"
+  amountText: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  priceText: {
+    fontSize: 36,
+    fontWeight: "400",
+    marginBottom: 5
+  },
+  walletAdd: {
+    fontSize: 12,
+    fontWeight: "400"
+  },
+  ethImg: {
+    height: 52,
+    width: 52,
+    resizeMode: "contain"
+  },
+  inputContainer: {
+    marginTop: 25
+  },
+  mr10: {
+    marginLeft: 25,
+    marginBottom: 10
+  },
+  mt10: {
+    marginLeft: 25,
+    marginBottom: 10,
+    marginTop: 10
+  },
+  chooseContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 55,
+    borderColor: "#C4C4C4",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 20,
+    marginHorizontal: 5
+  },
+  priceImg: { width: 27, height: 27, resizeMode: "contain" },
+  inputPrice: { fontSize: 16 },
+  bidContainer: { justifyContent: "center", alignItems: "center", marginTop: 3 },
+  bidText: { fontSize: 12, color: "#939396" },
+  feeContainer: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingHorizontal: 5, marginTop: 10 },
+  feeSection: {
+    justifyContent: "center",
+    alignItems: "flex-start",
+    height: 55,
+    borderColor: "#C4C4C4",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 20,
+    width: 170
+  },
+  buttonBottom: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginBottom: 10,
+    marginTop: 20
   }
 });
-export default UploadMediaScreen;
 
-const Button = (params) => {
-  const btnStyle = {
-    backgroundColor: params.outline ? "#fff" : "#000",
-    borderColor: params.outline ? "#000" : "#fff",
-    borderWidth: 1
-  };
-  const btnText = {
-    color: params.outline ? "#000" : "#fff"
-  };
+const Input = (props) => {
   return (
-    <View style={buttonStyles.btnContainer}>
-      <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
-        <Text style={[buttonStyles.btnText, btnText]}>{params.buttonText}</Text>
-        <View style={styles.childrenContainer}>{params.children}</View>
-      </Pressable>
+    <View>
+      <TextInput
+        style={textStyles.input}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChangeText={(num) => props.setValue(num)}
+        placeholderTextColor="#000"
+        editable={props.editable !== false}
+      />
+      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
     </View>
   );
 };
 
-const buttonStyles = StyleSheet.create({
-  btnContainer: {
-    paddingTop: 10,
-    paddingHorizontal: 20,
-    justifyContent: "center",
-    marginTop: 20
-  },
-  btn: {
-    backgroundColor: "black",
-    height: 50,
-    width: "100%",
-    padding: 10,
-    paddingHorizontal: 25,
+const textStyles = StyleSheet.create({
+
+  input: {
+    backgroundColor: "#fff",
+    height: 53,
+    borderColor: "#C4C4C4",
+    color: "#000",
     borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "rgba(0, 0, 0, 0.2)",
-    elevation: 10,
-    flexDirection: "row"
+    fontSize: 14,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginHorizontal: 5
   },
-  btnText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold"
-  },
-  childrenContainer: {
-    justifyContent: "center",
-    alignItems: "center"
+  error: {
+    fontSize: 13,
+    color: "#FA060D",
+    paddingTop: 8
   }
 });
+
+const Button = (props) => {
+  return (
+    <TouchableHighlight onPress={props.onPress} underlayColor="#DDDDDD">
+      <View
+        style={[
+          btnStyles.button,
+          {
+            backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
+            height: props.height ? props.height : 49,
+            borderWidth: props.borderWidth ? props.borderWidth : 0,
+            borderColor: props.borderColor ? props.borderColor : "#000000"
+          }
+        ]}
+      >
+        <Text style={[btnStyles.text, { color: props.color ? props.color : "#ffffff" }]}>
+          {props.children}
+        </Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const btnStyles = StyleSheet.create({
+  button: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginVertical: 10,
+    width: 150
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 15
+  }
+});
+
+export default PlaceBidScreen;
